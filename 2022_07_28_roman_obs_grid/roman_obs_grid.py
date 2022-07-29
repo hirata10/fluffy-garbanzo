2b56dc545ddf4b05acffb8dc91ac89c2e099f0c9
Masaya Yamamoto <my137@duke.edu>
2022-07-29 16:43:51 -0400

    BUG? star flux

--- 2022_07_28_roman_obs_grid/roman_obs_grid.py
+++ 2022_07_28_roman_obs_grid/roman_obs_grid.py
@@ -34,7 +34,7 @@ def _make_grid_image(nx_tiles, ny_tiles, stamp_xsize, stamp_ysize, config, psf, 
     pixel_scale = config['s_in']
     # Let's do point sources first. 
     # gal = galsim.Exponential(flux=1., half_light_radius=0.1)
-    st_model = galsim.DeltaFunction(flux=1.)
+    st_model = galsim.DeltaFunction(flux=1.e4)
     sky_level = 1.e6
     
     # Make an image where postage stamps fall in.
